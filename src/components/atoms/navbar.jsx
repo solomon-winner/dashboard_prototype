7634bd5ee199fb5d12f9ea6908a701069fd39830
solomon-winner <solyalew3@gmail.com>
2024-10-21 21:35:58 +0300

    ...

--- src/components/atoms/navbar.jsx
+++ src/components/atoms/navbar.jsx
@@ -1,3 +1,6 @@
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { IoLogOutOutline } from "react-icons/io5";
+
 export const NavBar = () => {
     return(
         <div className="text-2xl font-bold my-4 align-text-bottom justify-around ml-auto flex w-[10%]" id="bnav">
