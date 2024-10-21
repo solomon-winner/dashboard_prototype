24b58e3c5d6b0131d106ea488ae6c71f95562f23
solomon-winner <solyalew3@gmail.com>
2024-10-21 21:41:48 +0300

    main layout component...

---
+++ src/components/templates/mainLayout.js
@@ -0,0 +1,18 @@
+import { NavBar } from "../atoms/navbar"
+import SideBar from "../organisms/sideBar"
+
+const Layout = () => {
+    return (
+        <div className="flex">
+            <SideBar />
+            <div className="flex-1">
+                <NavBar />
+                <div className="p-4">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
