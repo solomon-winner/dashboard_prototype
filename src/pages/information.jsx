5c6def97f34cb36aa936651ed8be4ad1c424d398
solomon-winner <solyalew3@gmail.com>
2024-10-22 22:37:17 +0300

    ...

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -1,6 +1,6 @@
 const Information = () => {
     return (
-        <div className="ml-[15rem] bg-white-100 min-h-screen border ">
+        <div className="ml-[15rem] bg-white-100 min-h-screen ">
             <div className="flex flex-col justify-between gap-10% items-center">
 
     <div className="flex justify-center items-center gap-[10%] w-full h-screen">
@@ -11,9 +11,7 @@ const Information = () => {
                 Come to worship him with thankful hearts and songs of praise.
                 the LORD is the greatest God, king over all other gods..."
             </p>
-            <div className="text-white bg-green-600 w-[150px] h-[40px] flex items-center justify-center rounded-full">
-                Explore More
-            </div>
+            
         </div>
           <div
         className="w-[500px] h-[80vh] bg-cover bg-center border-b-[15px] border-[#185601] rounded-b-[60%]">
@@ -21,6 +19,43 @@ const Information = () => {
       </div>
     </div>
 
+    </div>
+
+    <div className="w-full h-auto flex justify-center max-w-full box-border">
+            <div className="w-[30%] h-auto m-0 mx-1 bg-white">
+                <div className="text-lg font-bold text-green-900 m-2 border-b border-green-900">
+                    Join the Mission of Spreading God's Love
+                </div>
+                <div className="text-lg text-green-900 p-2">
+                    Become a partner in sharing the gospel through music. Whether through prayer, financial support, or simply sharing the message, you can help spread the transformative power of worship to people around the world.
+                </div>
+            </div>
+            <div className="w-[30%] h-auto m-0 mx-1 bg-white">
+                <div className="text-lg font-bold text-green-900 m-2 border-b border-green-600">
+                    Join the Mission of Spreading God's Love
+                </div>
+                <div className="text-lg text-green-900 p-2">
+                    Become a partner in sharing the gospel through music. Whether through prayer, financial support, or simply sharing the message, you can help spread the transformative power of worship to people around the world.
+                </div>
+            </div>
+            <div className="w-[30%] h-auto m-0 mx-1 bg-white">
+                <div className="text-lg font-bold text-green-900 m-2 border-b border-green-600">
+                    Join the Mission of Spreading God's Love
+                </div>
+                <div className="text-lg text-green-900 p-2">
+                    Become a partner in sharing the gospel through music. Whether through prayer, financial support, or simply sharing the message, you can help spread the transformative power of worship to people around the world.
+                </div>
+            </div>
+        </div>
+
+        <div className="mt-32 w-full flex justify-center items-center gap-[10%] h-auto">
+      <div className="w-2/5 h-[500px] bg-white bg-cover bg-center"></div>
+      <div className="w-2/5 p-2 flex flex-col items-center justify-center text-green-900 font-sans h-[500px]">
+        <h2>About</h2>
+        <p>
+          Our company is dedicated to providing the best service possible to our customers. We strive to provide the best quality products and services to our customers. Our team is dedicated to providing the best customer service possible. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, molestias? Beatae repudiandae harum saepe voluptatibus illum. Expedita sit consequatur sapiente dolorum culpa, nam atque unde soluta officiis vitae similique enim! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus aliquid, laboriosam ipsum hic voluptatum molestias ipsa molestiae iusto corporis quas magni necessitatibus atque odit cum repellendus itaque explicabo ullam. Autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non neque fuga impedit ullam, dolore molestiae reiciendis facere ab omnis, qui fugiat, voluptatibus explicabo exercitationem! Mollitia architecto dignissimos nulla laboriosam quia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facere architecto, nostrum dicta voluptas animi, impedit sunt reiciendis natus fugiat possimus excepturi sed eveniet ratione consequatur consequuntur similique? Cupiditate, ducimus?
+        </p>
+      </div>
     </div>
  </div>   
              )
