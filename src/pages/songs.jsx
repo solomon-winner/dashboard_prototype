66945abae5f3fc983d218ad37c375d68f6ff0ea1
solomon-winner <solyalew3@gmail.com>
2024-10-22 13:35:07 +0300

    ...

--- src/pages/songs.jsx
+++ src/pages/songs.jsx
@@ -1,9 +1,10 @@
+import Albums from "../components/organisms/albumCards.jsx";
+
 const Song = () => {
     return (
-        <div className="ml-[15rem] bg-white-100 min-h-screen border ">
-            <div className="mt-11 border">
-             <h1>Songs Page</h1>
-               
+        <div className="ml-[15rem] bg-white-100 min-h-screen">
+            <div className="mt-11">
+                <Albums/>               
             </div>
             </div>    )
 }
