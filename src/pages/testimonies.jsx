bd3a449a637e7f7cdbf0bc0c8b52a56ac3d5e971
solomon-winner <solyalew3@gmail.com>
2024-10-22 15:55:35 +0300

    ...testimony page...

--- src/pages/testimonies.jsx
+++ src/pages/testimonies.jsx
@@ -1,11 +1,50 @@
 const Testimonies = () => {
     return (
-        <div className="ml-[15rem] bg-white-100 min-h-screen border ">
-            <div className="mt-11 border">
-             <h1>Testimonies Page</h1>
-               
+    <div className="ml-[13rem] bg-white-100 min-h-screen">
+      <div className=" max-w-5xl mx-auto p-6 ">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+        <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <div className="text-4xl font-bold text-blue-500 my-4" id="subscribersCount">300</div>
+            <h2 className="text-base font-semibold text-gray-800">Total Testimonies</h2>
+
+          </div>
+          <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <div className="text-4xl font-bold text-blue-500 my-4" id="testimoiesCount">200</div>
+            <h2 className="text-base font-semibold text-gray-800">Verified of Testimonies</h2>
+
+          </div>
+          <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <div className="text-4xl font-bold text-blue-500 my-4" id="testimoiesCount">90%</div>
+            <h2 className="text-base font-semibold text-gray-800">Percentage of Verified Testimonies</h2>
+
+          </div>
+          {/* <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <h2 className="text-xl font-semibold text-gray-800">Total Songs</h2>
+            <div className="text-4xl font-bold text-blue-500 my-4" id="songCount">300</div>
+          </div> */}
+          {/* <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <h2 className="text-xl font-semibold text-gray-800">Number of Visitors</h2>
+            <div className="text-4xl font-bold text-blue-500 my-4" id="visitorCount">Loading...</div>
+            <div className="chart">
             </div>
+          </div> */}
+          {/* <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <h2 className="text-xl font-semibold text-gray-800">Subscriber Growth Rate</h2>
+            <div className="text-4xl font-bold text-green-500 my-4" id="subscriberRate">Loading...</div>
+            <div className="chart">
             </div>
+          </div> */}
+          {/* <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
+            <h2 className="text-xl font-semibold text-gray-800">Content Engagement</h2>
+            <div className="text-4xl font-bold text-red-500 my-4" id="engagementRate">Loading...</div>
+            <div className="chart">
+            </div>
+          </div> */}
+         
+        </div>
+      </div>
+    </div>
     )
 }
 
