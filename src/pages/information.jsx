a668af20051cbf760f124868d27de17841b79b69
solomon-winner <solyalew3@gmail.com>
2024-10-22 22:18:58 +0300

    ...update information page...

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -1,11 +1,29 @@
 const Information = () => {
     return (
         <div className="ml-[15rem] bg-white-100 min-h-screen border ">
-            <div className="mt-11 border">
-             <h1>Information Page</h1>
-               
+            <div className="flex flex-col justify-between gap-10% items-center">
+
+    <div className="flex justify-center items-center gap-[10%] w-full h-screen">
+    <div className="flex flex-col items-center justify-end gap-1 w-[400px] h-[55vh] font-sans">
+            <h3>Psalms 95: 1-6</h3>
+            <p>
+                "... Sing joyful songs to the LORD! Praise the mighty rock where we are safe. 
+                Come to worship him with thankful hearts and songs of praise.
+                the LORD is the greatest God, king over all other gods..."
+            </p>
+            <div className="text-white bg-green-600 w-[150px] h-[40px] flex items-center justify-center rounded-full">
+                Explore More
             </div>
-            </div>    )
+        </div>
+          <div
+        className="w-[500px] h-[80vh] bg-cover bg-center border-b-[15px] border-[#185601] rounded-b-[60%]">
+
+      </div>
+    </div>
+
+    </div>
+ </div>   
+             )
 }
 
 export default Information;
