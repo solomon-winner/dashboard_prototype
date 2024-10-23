ae58e60653529870e19fa3910733b44e2ec86556
solomon-winner <solyalew3@gmail.com>
2024-10-23 13:54:33 +0300

    ...post page...

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -1,5 +1,8 @@
 import { useRef } from "react";
 import { IoSend } from "react-icons/io5";
+import { AiOutlineLike } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa6";
+import { FaTelegram } from "react-icons/fa";
 
 const Post = () => {
 
@@ -17,27 +20,38 @@ const Post = () => {
 
         <div className="w-[100%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
         <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
-            <div className="text-4xl font-bold text-green-900 my-4" id="subscribersCount">3</div>
+            <div className="text-4xl font-bold text-green-900 my-4" id="mediaCount">3</div>
             <h2 className="text-base font-semibold text-green-900">Social Medias</h2>
 
           </div>
           <div className="bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
-            <div className="text-4xl font-bold text-green-900 my-4" id="testimoiesCount">200</div>
+            <div className="text-4xl font-bold text-green-900 my-4" id="postCount">200</div>
             <h2 className="text-base font-semibold text-green-900">Total Posts</h2>
 
           </div>
           <div className=" bg-white rounded-lg shadow p-6 transition-transform transform hover:-translate-y-1">
-            <div className="text-4xl font-bold text-green-900 my-4" id="testimoiesCount">9000</div>
+            <div className="text-4xl font-bold text-green-900 my-4" id="likeCount">9000</div>
             <h2 className="text-base font-semibold text-green-900">Total Likes</h2>
 
           </div>
         </div>
       </div>
 
-      <div className="w-[95%] shadow min-h-32 mx-auto box-border p-1">
-        <div className="w-full text-green-800 px-6 ">
+      <div className="w-[95%] shadow min-h-32 mx-auto box-border p-1 ">
+        <div className="w-full text-green-800 px-6 flex justify-between items-end">
             05-09-2024
-        </div>
+         <div className="text-blue-600 w-fit gap-2 h-4 flex justify-center items-center">
+            <span className="text-green-900">Posted on :</span>
+            <div className="text-[#0165E1]">
+            <FaFacebook/>
+                
+            </div>
+            <div className="text-[#27A7E7]">
+            <FaTelegram/>
+
+            </div>
+            </div>
+       </div>
         <hr />
         <div className="w-full min-h-24 p-3 box-border italic text-green-800">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium exercitationem obcaecati quae officia rem! Autem, voluptatum nemo repellendus alias asperiores nobis sapiente impedit illo, incidunt qui maiores, praesentium deserunt a.
@@ -46,6 +60,7 @@ const Post = () => {
         </div>
         <div className="w-full h-auto flex text-left justify-end">
             <div className="w-fit h-auto flex gap-2 text-left px-2 pb-3">
+           <div className="w-20 h-8 flex gap-1 items-center mt-2 text-sm text-green-900 justify-center"><AiOutlineLike/> <span>100</span></div> 
             <button className="w-36 max-w-full h-8 flex items-center justify-center gap-1 bg-white text-green-600 text-sm font-semibold cursor-pointer border border-green-600 box-border mt-2 hover:text-red-700 ">Delete this Message</button>
             </div>
             </div>
@@ -53,9 +68,20 @@ const Post = () => {
 
 
       <div className="w-[95%] shadow min-h-32 mx-auto box-border p-1">
-        <div className="w-full text-green-800 px-6 ">
+        <div className="w-full text-green-800 px-6 flex justify-between items-end">
             05-09-2024
-        </div>
+         <div className="text-blue-600 w-fit gap-2 h-4 flex justify-center items-center">
+            <span className="text-green-900">Posted on :</span>
+            <div className="text-[#0165E1]">
+            <FaFacebook/>
+                
+            </div>
+            <div className="text-[#27A7E7]">
+            <FaTelegram/>
+
+            </div>
+            </div>
+       </div>
         <hr />
         <div className="w-full min-h-24 p-3 box-border italic text-green-800">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium exercitationem obcaecati quae officia rem! Autem, voluptatum nemo repellendus alias asperiores nobis sapiente impedit illo, incidunt qui maiores, praesentium deserunt a.
@@ -64,15 +90,28 @@ const Post = () => {
         </div>
         <div className="w-full h-auto flex text-left justify-end">
             <div className="w-fit h-auto flex gap-2 text-left px-2 pb-3">
+            <div className="w-20 h-8 flex gap-1 items-center mt-2 text-sm text-green-900 justify-center"><AiOutlineLike/> <span>100</span></div> 
+
             <button className="w-36 max-w-full h-8 flex items-center justify-center gap-1 bg-white text-green-600 text-sm font-semibold cursor-pointer border border-green-600 box-border mt-2 hover:text-red-700 ">Delete this Message</button>
             </div>
             </div>
       </div>
 
       <div className="w-[95%] shadow min-h-32 mx-auto box-border p-1">
-        <div className="w-full text-green-800 px-6 ">
+        <div className="w-full text-green-800 px-6 flex justify-between items-end">
             05-09-2024
-        </div>
+         <div className="text-blue-600 w-fit gap-2 h-4 flex justify-center items-center">
+            <span className="text-green-900">Posted on :</span>
+            <div className="text-[#0165E1]">
+            <FaFacebook/>
+                
+            </div>
+            <div className="text-[#27A7E7]">
+            <FaTelegram/>
+
+            </div>
+            </div>
+       </div>
         <hr />
         <div className="w-full min-h-24 p-3 box-border italic text-green-800">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium exercitationem obcaecati quae officia rem! Autem, voluptatum nemo repellendus alias asperiores nobis sapiente impedit illo, incidunt qui maiores, praesentium deserunt a.
@@ -81,6 +120,7 @@ const Post = () => {
         </div>
         <div className="w-full h-auto flex text-left justify-end">
             <div className="w-fit h-auto flex gap-2 text-left px-2 pb-3">
+            <div className="w-20 h-8 flex gap-1 items-center mt-2 text-sm text-green-900 justify-center"><AiOutlineLike/> <span>100</span></div> 
             <button className="w-36 max-w-full h-8 flex items-center justify-center gap-1 bg-white text-green-600 text-sm font-semibold cursor-pointer border border-green-600 box-border mt-2 hover:text-red-700 ">Delete this Message</button>
             </div>
             </div>
@@ -92,7 +132,7 @@ const Post = () => {
           <textarea
             ref={textareaRef}
             className="w-[95%] h-8 max-h-80 p-2 focus:outline-none focus:border-none resize-none overflow-y-auto"
-            placeholder="Send message to the subscribers via email"
+            placeholder="Post Your Idea Here"
             onInput={handleInput}
             rows="1"
           />
