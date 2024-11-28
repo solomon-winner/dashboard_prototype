5924c50a5086c09efec9249d3d4863cc88ec7188
solomon-winner <solyalew3@gmail.com>
2024-11-28 12:13:45 +0300

    ...dashboard...

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-
+import {
+    isEditingState,
+    titleState,
+    contentState,
+    companyInfoState,
+    cardInfoState,
+    cardTitleState,
+  } from '../state/state';
+  
  const Information = () => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [title, setTitle] = useState('Psalms 95: 1-6');
-    const [content, setContent] = useState('"... Sing joyful songs to the LORD! Praise the mighty rock where we are safe. Come to worship him with thankful hearts and songs of praise. the LORD is the greatest God, king over all other gods..."');
-    const [companyInfo, setCompanyInfo] = useState('Our company is dedicated to providing the best service possible to our customers. We strive to provide the best quality products and services to our customers. Our team is dedicated to providing the best customer service possible. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, molestias? Beatae repudiandae harum saepe voluptatibus illum. Expedita sit consequatur sapiente dolorum culpa, nam atque unde soluta officiis vitae similique enim! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus aliquid, laboriosam ipsum hic voluptatum molestias ipsa molestiae iusto corporis quas magni necessitatibus atque odit cum repellendus itaque explicabo ullam. Autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non neque fuga impedit ullam, dolore molestiae reiciendis facere ab omnis, qui fugiat, voluptatibus explicabo exercitationem! Mollitia architecto dignissimos nulla laboriosam quia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facere architecto, nostrum dicta voluptas animi, impedit sunt reiciendis natus fugiat possimus excepturi sed eveniet ratione consequatur consequuntur similique? Cupiditate, ducimus?');
-    const [CardInfo, setCardInfo] = useState('Become a partner in sharing the gospel through music. Whether through prayer, financial support, or simply sharing the message, you can help spread the transformative power of worship to people around the world.');
-    const [CardTitle, setCardTitle] = useState('Join the Mission of Spreading God\'s Love');
-
+    const [isEditing, setIsEditing] = useRecoilState(isEditingState);
+    const [title, setTitle] = useRecoilState(titleState);
+    const [content, setContent] = useRecoilState(contentState);
+    const [companyInfo, setCompanyInfo] = useRecoilState(companyInfoState);
+    const [CardInfo, setCardInfo] = useRecoilState(cardInfoState);
+    const [CardTitle, setCardTitle] = useRecoilState(cardTitleState);
+  
     const handleDivClick = () => {
         setIsEditing(true);
     };
--- src/state/state.js
+++ src/state/state.js
@@ -23,3 +23,33 @@ export const formTypeState = atom({
   key: 'formTypeState',
   default: 'update',
 });
+
+export const isEditingState = atom({
+  key: 'isEditingState',
+  default: false,
+});
+
+export const titleState = atom({
+  key: 'titleState',
+  default: 'Psalms 95: 1-6',
+});
+
+export const contentState = atom({
+  key: 'contentState',
+  default: '"... Sing joyful songs to the LORD! Praise the mighty rock where we are safe. Come to worship him with thankful hearts and songs of praise. the LORD is the greatest God, king over all other gods..."',
+});
+
+export const companyInfoState = atom({
+  key: 'companyInfoState',
+  default: 'Our company is dedicated to providing the best service possible to our customers. We strive to provide the best quality products and services to our customers. Our team is dedicated to providing the best customer service possible. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, molestias? Beatae repudiandae harum saepe voluptatibus illum. Expedita sit consequatur sapiente dolorum culpa, nam atque unde soluta officiis vitae similique enim! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus aliquid, laboriosam ipsum hic voluptatum molestias ipsa molestiae iusto corporis quas magni necessitatibus atque odit cum repellendus itaque explicabo ullam. Autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non neque fuga impedit ullam, dolore molestiae reiciendis facere ab omnis, qui fugiat, voluptatibus explicabo exercitationem! Mollitia architecto dignissimos nulla laboriosam quia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facere architecto, nostrum dicta voluptas animi, impedit sunt reiciendis natus fugiat possimus excepturi sed eveniet ratione consequatur consequuntur similique? Cupiditate, ducimus?',
+});
+
+export const cardInfoState = atom({
+  key: 'cardInfoState',
+  default: 'Become a partner in sharing the gospel through music. Whether through prayer, financial support, or simply sharing the message, you can help spread the transformative power of worship to people around the world.',
+});
+
+export const cardTitleState = atom({
+  key: 'cardTitleState',
+  default: 'Join the Mission of Spreading God\'s Love',
+});
