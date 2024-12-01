42218fc5556e633bc264efdb4cbff02b37bce17a
solomon-winner <solyalew3@gmail.com>
2024-12-01 20:20:04 +0300

    ...

--- src/utils/api.js
+++ src/utils/api.js
@@ -37,7 +37,7 @@ const deleteMenu = async (id) => {
 export const useIncrementVisitor = () => {
   return useQuery({
     queryKey: 'incrementVisitorCount',
-    queryFn: fetchMenus,
+    queryFn: incrementVisitorCount,
   });
 };
 
